f1c7527ed3b1943db7bfb2895f0517f4424ac09a
unknown <muhamadrivai338@gmail.com>
2016-06-24 04:34:04 -0700

    prak p4

---
+++ Praktikum4/com.praktikum_praktikum4_war_1.0-SNAPSHOT/src/main/java/com/praktikum/praktikum4/ProdukDao.java
@@ -0,0 +1,15 @@
+
+package com.praktikum.praktikum4;
+import javax.sql.DataSource;
+import java.util.*;
+public class ProdukDao {
+    
+    public void simpan(Produk p){
+    
+    }
+    
+    public Integer cariSemuaProduk(Integer id){
+        return id;
+    }
+    
+}
